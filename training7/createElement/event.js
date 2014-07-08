485e2bd81d99f14689d503c81ab118f5bbb3901b
knitta0331 <k-nitta.sys-link.jp>
2014-07-09 00:37:57 +0900

    #6

--- training7/createElement/event.js
+++
@@ -1,6 +0,0 @@
-function OK(){
-    var text = document.getElementById('txt_in').value;
-    document.getElementById('out').innerHTML = text;
-    }
-    var push = document.getElementById('btn');
-    push.onclick = OK;
